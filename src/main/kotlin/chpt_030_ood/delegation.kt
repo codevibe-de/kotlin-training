162da614d8dc7267c908358eb52d249b1228d2e2
Thomas Auinger <thomas@auinger.de>
2023-07-10 21:08:05 +0200

    030/delegation

---
+++ src/main/kotlin/chpt_030_ood/delegation.kt
@@ -0,0 +1,13 @@
+package chpt_030_ood
+
+class AuditingList<E>(private val delegate:MutableList<E>) : MutableList<E> by delegate {
+    override fun add(element: E): Boolean {
+        println("Adding element $element")
+        return delegate.add(element)
+    }
+}
+
+fun main() {
+    val list = AuditingList(mutableListOf<String>())
+    list.add("one")
+}
