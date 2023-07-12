064fabb30fb761fe1f497823e33eafa8bbf9bdb3
Thomas Auinger <thomas@auinger.de>
2023-07-12 07:42:55 +0200

    more factory

--- src/main/kotlin/chpt_051/abstract-factory.kt
+++ src/main/kotlin/chpt_051/abstract-factory.kt
@@ -1,11 +1,33 @@
 package chpt_051
 
-interface HQ<T,U> {
-    fun buildBarracks(): Building<T, U>
+interface HQ<T> {
+    fun buildBarracks(): Building<T, Infantry>
 }
 
-class CommandCenter : HQ<InfantryType, Infantry> {
-    override fun buildBarracks(): Barracks {
+class CommandCenter : HQ<InfantryType> {
+    override fun buildBarracks(): Building<InfantryType, Infantry> {
         return Barracks()
     }
 }
+
+class Hive : HQ<ZergInfantryType> {
+    override fun buildBarracks(): Building<ZergInfantryType, Infantry> {
+        return SpawningPool()
+    }
+
+}
+
+enum class ZergInfantryType {
+    ZERGLING
+}
+
+class SpawningPool : Building<ZergInfantryType, Infantry> {
+    override fun build(type: ZergInfantryType): Infantry {
+        return when(type) {
+            ZergInfantryType.ZERGLING -> Zergling()
+        }
+    }
+
+}
+
+class Zergling : Infantry
