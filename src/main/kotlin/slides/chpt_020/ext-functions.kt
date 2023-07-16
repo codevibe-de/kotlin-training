037bdfff870f4b84d729dc4d6122d960445c24b7
Thomas Auinger <thomas@auinger.de>
2023-07-16 06:34:38 +0200

    extension & typealias

---
+++ src/main/kotlin/slides/chpt_020/ext-functions.kt
@@ -0,0 +1,13 @@
+package slides.chpt_020
+
+inline fun <T> List<T>.replaceWithNulls(predicate: (T) -> Boolean): List<T?> {
+   return this
+        .map { if (predicate(it)) null else it }
+        .toList()
+}
+
+fun main() {
+    listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+        .replaceWithNulls { n -> n % 3 == 0 }
+        .also { println(it) }
+}
---
+++ src/main/kotlin/slides/chpt_030_ood/ext-properties.kt
@@ -0,0 +1,10 @@
+package slides.chpt_030_ood
+
+val List<Any>.lastIndex: Int
+    get() = size - 1
+
+fun main() {
+    listOf(1,2,3)
+        .lastIndex
+        .also { println(it) }
+}
