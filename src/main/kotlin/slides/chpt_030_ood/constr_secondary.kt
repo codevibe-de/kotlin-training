ad1b3ee5aa443fbce2344ac91120ae3398ce0484
Thomas Auinger <thomas@auinger.de>
2023-07-17 18:24:17 +0200

    030 constr files reorg

---
+++ src/main/kotlin/slides/chpt_030_ood/constr_secondary.kt
@@ -0,0 +1,22 @@
+package slides.chpt_030_ood
+
+class Animal3 (
+    val name: String
+) {
+    constructor(order: String, family: String)
+            : this("animal of Order `$order` and family `$family`")
+
+    init {
+        println("Init $name")
+    }
+
+    fun introduce() {
+        println("Hi, I am a $name")
+    }
+}
+
+fun main() {
+    Animal3("bee").introduce()
+    Animal3("Rodentia", " Muridae").introduce()
+}
+
