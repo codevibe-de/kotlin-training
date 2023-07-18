597524df9ef54e567f40fabaca852d0a2692cde7
Thomas Auinger <thomas@auinger.de>
2023-07-18 16:02:49 +0200

    exrc 030-q prep

---
+++ src/main/kotlin/exrc/chpt_030/q_vector.kt
@@ -0,0 +1,21 @@
+package exrc.chpt_030
+
+import org.assertj.core.api.Assertions.assertThat
+
+fun main() {
+//    var v = Vector(0,0)
+//    v += Vector(10, 5)
+//    assertThat(v).hasToString("Vector(x=10, y=5)")
+//    v -= Vector(5,1)
+//    assertThat(v).hasFieldOrPropertyWithValue("x", 5)
+//    assertThat(v).hasFieldOrPropertyWithValue("y", 4)
+//    v = -v
+//    assertThat(v).isEqualTo(Vector(-5,-4))
+//    val (x, y) = v
+//    assertThat(x).isEqualTo(-5)
+//    assertThat(y).isEqualTo(-4)
+//    val x2 = v['x']
+//    val y2 = v['y']
+//    assertThat(x2).isEqualTo(-5)
+//    assertThat(y2).isEqualTo(-4)
+}
