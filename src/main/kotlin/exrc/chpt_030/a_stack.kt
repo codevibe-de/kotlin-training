cb740deb69b7d4cd2527efe30a0d93c4cd6918b5
Thomas Auinger <thomas@auinger.de>
2023-07-18 07:53:42 +0200

    sltn 030 a + c

--- src/main/kotlin/exrc/chpt_030/a_stack.kt
+++ src/main/kotlin/exrc/chpt_030/a_stack.kt
@@ -3,19 +3,31 @@ package exrc.chpt_030
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
 
+class Stack<T> {
+
+    private val _items = mutableListOf<T>()
+
+    fun push(item: T) = _items.add(item)
+
+    fun peek() = _items.last()
+
+    fun pop() = _items.removeLast()
+
+}
+
 fun main() {
-//    val stack = Stack() // oder Stack<String>()
-//    stack.push("a")
-//    stack.push("b")
-//    stack.push("c")
-//    assertThat(stack.peek()).isEqualTo("c")
-//    stack.pop()
-//    assertThat(stack.peek()).isEqualTo("b")
-//    stack.push("d")
-//    assertThat(stack.peek()).isEqualTo("d")
-//    stack.pop()
-//    stack.pop()
-//    stack.pop()
-//    assertThatExceptionOfType(NoSuchElementException::class.java)
-//        .isThrownBy { stack.pop() }
+    val stack = Stack<String>()
+    stack.push("a")
+    stack.push("b")
+    stack.push("c")
+    assertThat(stack.peek()).isEqualTo("c")
+    stack.pop()
+    assertThat(stack.peek()).isEqualTo("b")
+    stack.push("d")
+    assertThat(stack.peek()).isEqualTo("d")
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    assertThatExceptionOfType(NoSuchElementException::class.java)
+        .isThrownBy { stack.pop() }
 }
--- src/main/kotlin/exrc/chpt_030/c_constructor.kt
+++ src/main/kotlin/exrc/chpt_030/c_constructor.kt
@@ -1,9 +1,17 @@
 package exrc.chpt_030
 
-class SuperString
+class SuperString(val value:String) {
+
+    constructor(ch:Char, count:Int) : this(ch.toString().repeat(count))
+
+    constructor(unicode:Int) : this(unicode.toChar().toString())
+
+    override fun toString() = value
+
+}
 
 fun main() {
-//    println(SuperString("hallo"))
-//    println(SuperString('-', 30))
-//    println(SuperString(3000))
+    println(SuperString("hallo"))
+    println(SuperString('-', 30))
+    println(SuperString(3000))
 }
