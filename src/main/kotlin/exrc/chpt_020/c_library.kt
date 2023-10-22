c2442e17bb967b34ece1bf02625fd6949d42a8ec
Thomas Auinger <thomas@auinger.de>
2023-10-22 18:37:24 +0200

    added bonus to c and incl multi-line-string

--- src/main/kotlin/exrc/chpt_020/c_library.kt
+++ src/main/kotlin/exrc/chpt_020/c_library.kt
@@ -6,12 +6,30 @@ fun addBook(
     price: Float,
     author: String = "??"
 ) {
-    println("Adding book '$title' (written by $author), having ISBN $isbn and price $price")
+    var output = """
+            ADDING BOOK '$title'
+            - written by $author
+            - ISBN $isbn
+            - price $price
+          """.trimIndent()
+    println()
+    println(output)
 }
 
-fun main() {
+fun main(args: Array<String>) {
     addBook("Stark", "123-123", 7.99f, "Ben Elton")
     addBook("Stark", "123-123", 7.99f)
     addBook("Stark", price = 7.99f)
     addBook(title = "Stark", price = 7.99f, author = "Ben Elton")
+
+    // bonus:
+    for (arg in args) {
+        val parts = arg.split(":")
+        val (titleString, priceString) = parts
+        try {
+            addBook(title = titleString, price = priceString.toFloat())
+        } catch (e: NumberFormatException) {
+            println("\nSkipping book '$titleString' with malformed price")
+        }
+    }
 }
