c022218997cedbd06066f5de517adc777c2e2e6b
Thomas Auinger <thomas@auinger.de>
2023-10-22 19:41:43 +0200

    renamed j) and added all new solutions

---
+++ src/main/kotlin/exrc/chpt_020/j_ext-functions.kt
@@ -0,0 +1,35 @@
+package exrc.chpt_020
+
+fun String.vowelCount(): Int {
+    val vowels = setOf('a','ä','A','Ä','e','E','i','I','o','O','ö','Ö','u','U','ü','Ü')
+    var result = 0
+    for (c in this) {
+        result += if (c in vowels) 1 else 0
+    }
+    return result
+}
+
+
+fun String.camelcase(): String {
+    val words = this.split(' ').toMutableList()
+    for ((i,w) in words.withIndex()) {
+        words[i] = w[0].uppercase() + w.substring(1).lowercase()
+    }
+    return words.joinToString("")
+}
+
+
+fun String.spaced(): String {
+    val result = StringBuilder()
+    for (c in this) {
+        result.append(c).append(' ')
+    }
+    return result.toString()
+}
+
+
+fun main() {
+    println("Übungen".vowelCount()) // 3
+    println("Wir lernen Kotlin".camelcase()) // WirLernenKotlin
+    println("Eine wichtige Überschrift".spaced()) // E i n e   w i c h t i g e   Ü b e r s c h r i f t
+}
