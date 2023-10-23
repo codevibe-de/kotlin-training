1b2071653192222c4863c98ebfd3e3c0d0a13f3d
Thomas Auinger <thomas@auinger.de>
2023-10-23 17:12:29 +0200

    030-g fixed faulty prio

--- src/main/kotlin/exrc/chpt_030/g_task.kt
+++ src/main/kotlin/exrc/chpt_030/g_task.kt
@@ -6,7 +6,7 @@ fun main() {
 //    val task1 = Task("Do something")
 //    Assertions.assertThat(task1.priority).isEqualTo(Task.DEFAULT_PRIO)
 //
-//    val task2 = Task("Clean room", -1000)
+//    val task2 = Task("Clean room", -99)
 //    Assertions.assertThat(task2.priority).isEqualTo(1)
 }
 
