068ca14d717d9e36df0bd051d0689f664911c8da
Thomas Auinger <thomas@auinger.de>
2023-10-24 10:04:40 +0200

    sltn 030-r

--- src/main/kotlin/exrc/chpt_030/r_lazy.kt
+++ src/main/kotlin/exrc/chpt_030/r_lazy.kt
@@ -3,8 +3,31 @@ package exrc.chpt_030
 import java.util.UUID
 
 fun main() {
-//    val task = IdentifiableTask("test")
-//    println(task)
-//    println(task.id)
+    val task = IdentifiableTask("test")
+    println(task)
+    println(task.id)
 }
 
+
+class IdentifiableTask(
+    val name: String,
+    initialPriority: Byte = DEFAULT_PRIO
+) {
+    var priority: Byte = validPriority(initialPriority)
+        set(value) {
+            field = validPriority(value)
+        }
+
+    val id by lazy {
+        println("Generating id...")
+        UUID.randomUUID().toString()
+    }
+
+    private fun validPriority(p: Byte) = p.coerceIn(MIN_PRIO, MAX_PRIO)
+
+    companion object {
+        const val MIN_PRIO: Byte = 1
+        const val MAX_PRIO: Byte = 5
+        const val DEFAULT_PRIO: Byte = 3
+    }
+}
