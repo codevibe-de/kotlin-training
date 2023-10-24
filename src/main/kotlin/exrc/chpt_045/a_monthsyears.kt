beae4ebe2d0275891c27d62e062f535e55408d75
Thomas Auinger <thomas@auinger.de>
2023-10-24 16:01:45 +0200

    exrc 045 update

--- src/main/kotlin/exrc/chpt_045/a_monthsyears.kt
+++ src/main/kotlin/exrc/chpt_045/a_monthsyears.kt
@@ -1,7 +1,13 @@
 package exrc.chpt_045
 
 import java.time.LocalDate
+import java.time.Month
+import java.time.format.TextStyle
+import java.util.Locale
 
 fun main() {
     var prevDate: LocalDate? = null
 }
+
+fun nameOfMonth(month:Int) =
+    Month.of(month).getDisplayName(TextStyle.FULL, Locale.getDefault())
