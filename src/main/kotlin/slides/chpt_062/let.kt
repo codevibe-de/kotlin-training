e79d2fe305e0fd561c64d684c09e6ad8f756a55e
Thomas Auinger <thomas@auinger.de>
2023-11-13 13:44:44 +0100

    improved 062 scope fcts

--- src/main/kotlin/slides/chpt_062/let.kt
+++ src/main/kotlin/slides/chpt_062/let.kt
@@ -5,16 +5,11 @@ import kotlin.random.Random
 // public inline fun <T, R> T.let(block: (T) -> R): R
 
 fun main() {
-    val str: String? = generateStringOrNot()
-
-    str?.let {
-        println("Hey, we got the string '$it'")
-    }
-
-    val hash: Int = str?.let { it.hashCode() } ?: 0
+    val hashCode = generateStringOrNull()
+        ?.let { it.hashCode() } ?: 0
 }
 
-fun generateStringOrNot() =
+fun generateStringOrNull() =
     when (Random.nextBoolean()) {
         true -> "hello!"
         false -> null
--- src/main/kotlin/slides/chpt_062/run.kt
+++ src/main/kotlin/slides/chpt_062/run.kt
@@ -1,21 +1,25 @@
 package slides.chpt_062
 
+import java.awt.Image
+import java.awt.image.BufferedImage
+
 // public inline fun <T, R> T.run(block: T.() -> R): R
 
 class Turtle {
-    fun penDown() {}
-    fun penUp() {}
     fun turn(degrees: Double) {}
     fun forward(pixels: Double) {}
+    fun export(): Image {
+        return BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB)
+    }
 }
 
 fun main() {
-    Turtle().run { //draw a 100 pix square
-        penDown()
-        for (i in 1..4) {
-            forward(100.0)
-            turn(90.0)
+    val image =
+        Turtle().run { //draw a 100 pix square
+            for (i in 1..4) {
+                forward(100.0)
+                turn(90.0)
+            }
+            export() // return value
         }
-        penUp()
-    }
 }
