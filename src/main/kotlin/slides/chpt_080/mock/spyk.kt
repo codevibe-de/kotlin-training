f140904d5ed5445100738f026aae0c2d321619e6
Thomas Auinger <thomas@auinger.de>
2023-11-13 14:50:00 +0100

    improved spyk

--- src/main/kotlin/slides/chpt_080/mock/spyk.kt
+++ src/main/kotlin/slides/chpt_080/mock/spyk.kt
@@ -5,15 +5,15 @@ import io.mockk.spyk
 
 fun main() {
     val someService = spyk(SomeService())
-    every { someService.foo(more(0)) } returns "mocked"
+    every { someService.foo(more(10, true)) } returns "mocked"
     every { someService.foo(100) } answers { callOriginal() }
 
     println(someService.foo())
-    println(someService.foo(1))
+    println(someService.foo(10))
     println(someService.foo(100))
 }
 
 
 private class SomeService {
-    fun foo(n: Int = 0) = "foo..."
+    fun foo(n: Int = 0) = "original"
 }
