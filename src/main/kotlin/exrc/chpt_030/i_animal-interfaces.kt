9d5698f3a461fad8fcad9c89f2802b88559236ee
Thomas Auinger <thomas@auinger.de>
2023-11-13 18:38:13 +0100

    sltn: removed e), added i)

--- src/main/kotlin/exrc/chpt_030/i_animal-interfaces.kt
+++ src/main/kotlin/exrc/chpt_030/i_animal-interfaces.kt
@@ -1,6 +1,39 @@
 package exrc.chpt_030
 
-interface Finned
+interface Finned {
+    val finCount: Int
+}
 
-interface Toothed
+interface Toothed {
+    fun bite(animal: Animal)
+}
 
+abstract class Animal {
+    abstract val name: String
+}
+
+
+open class Mammal(
+    override val name: String
+) : Animal()
+
+
+class BlueWhale(
+    name: String,
+    override val finCount: Int
+) : Mammal(name), Finned
+
+class Lion(
+    name: String
+) : Mammal(name), Toothed {
+    override fun bite(animal: Animal) {
+        println("Biting ${animal.name}")
+    }
+}
+
+
+fun main() {
+    val lion = Lion("Leo")
+    val blueWhale = BlueWhale("Mobby", 4)
+    lion.bite(blueWhale)
+}
