33921401abb2443ccb7374492c33fceb6c88b8af
Thomas Auinger <thomas@auinger.de>
2023-11-15 10:05:34 +0100

    extnd operators.kt

--- src/main/kotlin/slides/chpt_030_ood/operators.kt
+++ src/main/kotlin/slides/chpt_030_ood/operators.kt
@@ -1,6 +1,13 @@
 package slides.chpt_030_ood
 
 fun main() {
+    // motivation
+    val l = listOf(1, 2)
+    val l2 = l + listOf(3, 4)
+    val item = l2[3]    // 4
+    val same = l2 == listOf(1, 2, 3, 4) // true
+
+    // 2nd slide
     var counter = Counter(0)
     println(counter++) // count=0
     println(-counter) // count=-1
