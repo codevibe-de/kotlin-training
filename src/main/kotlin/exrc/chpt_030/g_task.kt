4ecdfd6961814c177c26bbcb30e38d2dfe5af9dd
Thomas Auinger <thomas@auinger.de>
2025-07-22 15:59:03 +0200

    030-g using check insteadof assertj

--- src/main/kotlin/exrc/chpt_030/g_task.kt
+++ src/main/kotlin/exrc/chpt_030/g_task.kt
@@ -1,15 +1,12 @@
 package exrc.chpt_030
 
-//import org.assertj.core.api.Assertions.assertThat
-
 fun main() {
 //    val task1 = Task("Do something")
-//    assertThat(task1.priority).isEqualTo(Task.DEFAULT_PRIO)
+//    check(task1.priority == Task.DEFAULT_PRIO)
 //
 //    val task2 = Task("Clean room", -99)
-//    assertThat(task2.priority).isEqualTo(1)
+//    check(task2.priority == 1)
 //
 //    task2.priority = 100 // yeah, let's clean up!
-//    Assertions.assertThat(task2.priority).isEqualTo(5)
+//    check(task2.priority == 5)
 }
-
