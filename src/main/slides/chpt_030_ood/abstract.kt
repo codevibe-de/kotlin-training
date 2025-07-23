b743c0df100c9f6c615d6006b2c3aa6609b3140e
Thomas Auinger <thomas@auinger.de>
2025-07-23 11:36:16 +0200

    slide code

--- src/main/slides/chpt_030_ood/abstract.kt
+++ src/main/slides/chpt_030_ood/abstract.kt
@@ -2,12 +2,14 @@ package chpt_030_ood
 
 import java.util.*
 
+// Ich manage das "length" property, damit es kein anderer machen muss
 abstract class IdGenerator(protected val length: Int) {
     abstract val name: String
     abstract fun generateId(): String
 }
 
 
+// Ich stelle (wie auch immer) einen Namen zur Verfügung
 class UUidGenerator(
     override val name: String,
     length: Int
@@ -18,4 +20,10 @@ class UUidGenerator(
             .toString()
             .substring(0, length)
     }
+}
+
+class XyGenerator(length: Int) : IdGenerator(length) {
+    override val name: String
+        get() = "Xy"
+    override fun generateId(): String = ""
 }
